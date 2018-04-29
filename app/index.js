da5bcd20efbd0a0c3349fa7e951b3f24f3f74535
Amrish Jhaveri <ajhave5@uic.edu>
2018-04-28 21:21:23 -0500

    multiple file reading

--- app/index.js
+++ app/index.js
@@ -1,14 +1,27 @@
-
 const pipeKeywords = require('./keywords');
 
 const fs = require('fs');
 
-//console.log(JSON.stringify(pipeKeywords.keysArray));
-async function readFile() {
-
-    await fs.readFile('./../Jenkinsfiles/Jenkinsfile_3', 'utf8', function (err, contents) {
-        console.log(err);
-    });
-
+const CONSTANTS = {
+    JENKINS_SAMPLE: './jenksinsfile_samples.json',
+    JENKINSFILE_LOCATION_PREPEND: './Jenkinsfiles/'
 }
-readFile();
+
+function startProcess() {
+    var jenkinsFileArray = JSON.parse(fs.readFileSync(CONSTANTS.JENKINS_SAMPLE, 'utf8'));
+    jenkinsFileArray.map(readFileWrapper());
+    console.log(jenkinsFileArray);
+}
+
+function readFileWrapper() {
+    return async function readFile(eachJenkinsFile) {
+
+        await fs.readFile(CONSTANTS.JENKINSFILE_LOCATION_PREPEND + eachJenkinsFile.localName, 'utf8', function (err, contents) {
+            console.log(contents);
+
+        });
+
+    }
+}
+
+startProcess();
