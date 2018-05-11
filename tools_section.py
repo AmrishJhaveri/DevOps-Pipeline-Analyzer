3a9e2515d964e1b92d6f155a6ee5ad2277a51acb
Chinmay Gangal <chinmay2312@gmail.com>
2018-05-10 19:44:44 -0500

    Added tools_section files for question 5

--- tools_section.py
+++ tools_section.py
@@ -3,13 +3,13 @@ import matplotlib.pyplot as plt
 import json
 import operator
 
-with open('post_section.json') as f:
+with open('tools_section.json') as f:
     data = json.load(f)
 	
 sortedData = sorted(data.items(), key=operator.itemgetter(1))
 sortedData.reverse()
 	
-keyList = ["always","changed","fixed","regression","aborted","failure","success","unstable","cleanup"]
+keyList = ["maven","jdk","gradle"]
 missingKeys = list(set(keyList) - set(list(data.keys())))
 
 myKeys = [t[0] for t in sortedData]#list(data.keys())
@@ -18,10 +18,10 @@ myKeys.extend(missingKeys)
 counts = [t[1] for t in sortedData]#list(data.values())
 counts.extend([0]*len(missingKeys))
 
-fig = plt.figure(figsize=(9,max(counts)))
-plt.bar(np.arange(9),counts)
-plt.xticks(np.arange(9),myKeys)
-plt.xlabel('post-condition blocks')
+fig = plt.figure(figsize=(3,max(counts)))
+plt.bar(np.arange(3),counts)
+plt.xticks(np.arange(3),myKeys)
+plt.xlabel('Tools used')
 plt.ylabel('Frequency')
 
-fig.savefig('plots/post_section.jpg')
+fig.savefig('plots/tools_section.jpg')
