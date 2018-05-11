2ffe0afcee88f678a06368a33519c3d8297e5fd7
Chinmay Gangal <chinmay2312@gmail.com>
2018-05-10 19:20:29 -0500

    -fill missing values as 0 -produce bar chart in sorted order

---
+++ post_section.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import json
+import operator
+
+with open('post_section.json') as f:
+    data = json.load(f)
+	
+sortedData = sorted(data.items(), key=operator.itemgetter(1))
+sortedData.reverse()
+	
+keyList = ["always","changed","fixed","regression","aborted","failure","success","unstable","cleanup"]
+missingKeys = list(set(keyList) - set(list(data.keys())))
+
+myKeys = [t[0] for t in sortedData]#list(data.keys())
+myKeys.extend(missingKeys)
+
+counts = [t[1] for t in sortedData]#list(data.values())
+counts.extend([0]*len(missingKeys))
+
+fig = plt.figure(figsize=(9,max(counts)))
+plt.bar(np.arange(9),counts)
+plt.xticks(np.arange(9),myKeys)
+
+fig.savefig('post_section.jpg')
