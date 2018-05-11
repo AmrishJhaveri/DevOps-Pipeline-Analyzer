8f2734d381ecbce3fe1c2fc48dae3218a8c417cc
Amrish Jhaveri <ajhave5@uic.edu>
2018-05-11 18:39:06 -0500

    q4 addition

---
+++ app/q4/q4.js
@@ -0,0 +1,265 @@
+var request = require('request');
+
+// NPM module for GitHub Api
+const octokit = require('@octokit/rest')();
+
+// NPM module(Node internal) for file reading & writing operations
+const fs = require('fs');
+
+// NPM module for HTTP request
+const axios = require('axios');
+
+
+var parsedJenkinsFile = [];
+var parseBasedOnOutput = {
+  research_question_4:
+      'For stages in parallel, fail-fast feature is used for what type of operations(stage names)? When it is used/unused, what are the operations running in parallel?',
+  parallel_stages_analysis: [],
+  project_details: []
+}
+
+const CONSTANTS = {
+  JENKINS_FILE_INFO_WITH_REPO: './app/q4/finalOutput.json',
+//   INTERMEDIATE_OUTPUT_FOR_PYTHON: './app/q4/intermediateOutput.json',
+//   INTERMEDIATE_OUTPUT_FOR_PYTHON_2: './app/q4/intermediateOutput_2.json'
+}
+
+const SEARCH_CODE_GIT_CONSTANTS = {
+  REPOS_PER_PAGE: 40,
+  MAX_NO_OF_PAGES_TO_FETCH_FROM: 3,
+  RECURSIVE_CALLS_TO_JENKINS: 2
+}
+
+// Github Personal Access Token. Only Read access to public repos is provided
+// with this token.
+const accessToken = '76a1e4c56ede658b7bd1241ccfb51d6c5511f289';
+// Token setup for Octokit to increase the read limit to 5000 requests every
+// hour.
+octokit.authenticate({type: 'token', token: accessToken})
+
+/**
+ * This generates the JS object with the search parameters(q), sorting
+ * parameter(sort), ordering of result(order), page number of result(page), and
+ * no of requests per page(per_page). This object is the input for
+ * octokit.search.repos.
+ *
+ * @param {*} page_no page number of the search query to be considered for finding the repos
+ */
+function getParams(page_no) {
+  // Search Java language projects with MIT license
+  let q_param = 'Jenkinsfile in:path agent parallel in:file language:Groovy';
+  // let q_param = "mock language:java license:mit";
+  // Sort by the stars of the Github project
+  let sort_param = 'stars';
+  // Order in descending order
+  let order_param = 'desc';
+  // Get 5 projects from this page
+  let per_page_number = SEARCH_CODE_GIT_CONSTANTS.REPOS_PER_PAGE;
+
+  return params = {
+    q: q_param,
+    // sort: sort_param,
+    order: order_param,
+    // page: page_no,
+    per_page: per_page_number
+  }
+};
+
+/**
+ * Async function to fetch the repo data based on the input provided by
+ * getParams(). Uses octokit.search.repos to fetch the required repositories.
+ * Await is used to wait till the repo result is available.
+ * Once available then only fetch the pull requests data of each repo.
+ */
+async function startProcess() {
+  try {
+    // Make a HTTP call to fetch the Repo details using Octokit Git Api.
+    // Wait till the data is available.
+    let result_data = await paginateSearchCalls();
+    console.log('after startProcess()');
+
+    let promises = result_data.map(getEachJenkinsFileWrapper());
+    await Promise.all(promises);
+
+    promises = parsedJenkinsFile.map(eachParsedJenkinsFileWrapper());
+    await Promise.all(promises);
+
+    writeToFile();
+    // writeIntermediateFile();
+    // writeIntermediateFile2();
+
+  } catch (e) {
+    console.log('startProcess');
+    console.log(e);
+  }
+}
+
+/**
+ *
+ */
+function eachParsedJenkinsFileWrapper() {
+  return async function(eachFile) {
+    parseBasedOnOutput.project_details.push(eachFile);
+
+    if (eachFile.jenkins_pipeline && eachFile.jenkins_pipeline.pipeline &&
+        eachFile.jenkins_pipeline.pipeline.stages) {
+      let promises = eachFile.jenkins_pipeline.pipeline.stages.map(
+          processEachStageBlock());
+      await Promise.all(promises);
+    }
+  }
+}
+
+/**
+ *
+ */
+function processEachStageBlock() {
+  return async function(eachStageObj) {
+    let tempObj = {};
+
+    let stageName = eachStageObj.name.toLowerCase();
+    // let count = parseBasedOnOutput.parallel_stages_analysis[stageName];
+    // if (!parseBasedOnOutput.parallel_stages_analysis[stageName]) {
+    //   parseBasedOnOutput.parallel_stages_analysis[stageName] = 1;
+    // } else {
+    //   parseBasedOnOutput.parallel_stages_analysis[stageName] = count + 1;
+    // }
+    if (eachStageObj.parallel) {
+      tempObj['outer_stage_name'] = stageName;
+      tempObj['fail_fast'] = eachStageObj.failFast ? true : false;
+      const promises =
+          eachStageObj.parallel.map(processEachStageWrapper(tempObj));
+      await Promise.all(promises);
+      parseBasedOnOutput.parallel_stages_analysis.push(tempObj);
+    }
+  }
+}
+
+function processEachStageWrapper(tempObj) {
+  return async function(eachStageObj) {
+    if (!tempObj['parallel_stages']) {
+      tempObj['parallel_stages'] = [];
+    }
+    tempObj['parallel_stages'].push(eachStageObj.name);
+  }
+}
+
+/**
+ *
+ */
+function getEachJenkinsFileWrapper() {
+  return async function(eachRepoForFile) {
+    try {
+      let myJsonStructure = {};
+      let response = await axios.get(
+          eachRepoForFile.git_url + '?access_token=' + accessToken);
+
+      let fileContent = Buffer.from(response.data.content, 'base64');
+      let jsonResponse;
+      try {
+        jsonResponse = await jenkinsJSONPromise(fileContent);
+      } catch (e) {
+        // console.log('getEachJenkinsFileWrapper try');
+        console.log(e);
+      }
+
+
+      myJsonStructure['full_repo_name'] = eachRepoForFile.repository.full_name;
+      myJsonStructure['repo_url'] = eachRepoForFile.repository.html_url;
+      myJsonStructure['html_url_jenkinsfile'] = eachRepoForFile.html_url;
+      myJsonStructure['api_url_jenkinsfile'] = eachRepoForFile.git_url;
+      //   myJsonStructure['actual_jenkinsfile']=fileContent.toString('ascii');
+      myJsonStructure['jenkins_pipeline'] = jsonResponse;
+      parsedJenkinsFile.push(myJsonStructure);
+
+    } catch (e) {
+      console.log('getEachJenkinsFileWrapper');
+      console.log(e);
+    }
+  }
+}
+
+/**
+ *
+ * @param {*} fileContent
+ */
+function jenkinsJSONPromise(fileContent) {
+  return new Promise((resolve, reject) => {
+    let options = {
+      method: 'POST',
+      url: 'http://192.168.99.100:9080/pipeline-model-converter/toJson',
+      headers: {
+        'cache-control': 'no-cache',
+        authorization: 'Basic YWRtaW46MTIzNDU2Nzg5',
+        'content-type':
+            'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW',
+        'Connection': 'keep-alive'
+      },
+      formData: {jenkinsfile: fileContent}
+    };
+
+    let count = 0;
+    recursiveRequest(resolve, reject, options, count);
+  });
+}
+
+function recursiveRequest(resolve, reject, options, count) {
+  count++;
+  request(options, function(error, response, body) {
+    if (error) {
+      // console.log(error);
+      //   console.log(JSON.stringify(error));
+      if (count < SEARCH_CODE_GIT_CONSTANTS.RECURSIVE_CALLS_TO_JENKINS &&
+          error.code === 'ECONNRESET') {
+        recursiveRequest(resolve, reject, options, count);
+        return;
+      }
+      reject(error);
+      return;
+    } else if (JSON.parse(body).data.json) {
+      resolve(JSON.parse(body).data.json);
+    } else if (JSON.parse(body).data) {
+      resolve(JSON.parse(body).data.errors);
+    }
+  });
+}
+
+/**
+ *
+ */
+function writeToFile() {
+  console.log('File writing started.');
+  fs.writeFileSync(
+      CONSTANTS.JENKINS_FILE_INFO_WITH_REPO,
+      JSON.stringify(parseBasedOnOutput, undefined, 2));
+}
+
+function writeIntermediateFile() {
+  fs.writeFileSync(
+      CONSTANTS.INTERMEDIATE_OUTPUT_FOR_PYTHON,
+      JSON.stringify(
+          parseBasedOnOutput.parallel_stages_analysis, undefined, 2));
+}
+
+function writeIntermediateFile2() {
+  fs.writeFileSync(
+      CONSTANTS.INTERMEDIATE_OUTPUT_FOR_PYTHON_2,
+      JSON.stringify(
+          parseBasedOnOutput.counts_commands_in_stages_operations, undefined,
+          2));
+}
+
+async function paginateSearchCalls() {
+  let count_of_pages = SEARCH_CODE_GIT_CONSTANTS.MAX_NO_OF_PAGES_TO_FETCH_FROM;
+  let response = await octokit.search.code(getParams(1));
+  let data = [];
+  data = data.concat(response.data.items);
+  while (count_of_pages > 1 && octokit.hasNextPage(response)) {
+    response = await octokit.getNextPage(response);
+    data = data.concat(response.data.items);
+    count_of_pages--;
+  }
+  return data;
+}
+
+module.exports = {startProcess}
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // The module which contains the application of logic of fetching the data from
 // github api and cloning the repos.
-const app = require('./app/q3/q3.js');
+const app = require('./app/q4/q4.js');
 
 // Call the getRepoData() function in the module.
 app.startProcess();
