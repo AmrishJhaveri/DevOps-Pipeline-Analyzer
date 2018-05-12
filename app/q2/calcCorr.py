b07af7c32bfe1870a64f6248e508a8990cea7df0
Chinmay Gangal <chinmay2312@gmail.com>
2018-05-12 03:32:47 -0500

    calculating coefficient plotting scatter

--- app/q2/calcCorr.py
+++ app/q2/calcCorr.py
@@ -7,6 +7,8 @@ from scipy.stats.stats import pearsonr
 with open('intermediateOutput.json') as f:
     vals = json.load(f)
 	
+
+	
 triggerList = []
 stageCountList = []
 
@@ -14,4 +16,28 @@ for v in vals:
 	triggerList.append(v['triggers'])
 	stageCountList.append(v['stages'])
 
-pearsonr(triggerList,stageCountList)[0]
+corrCoef = round(pearsonr(triggerList,stageCountList)[0],2)
+
+with open('finalOutput.json','r+',encoding="utf8") as f:
+	full = json.load(f)
+	full["corelation_coefficient"] = corrCoef
+	f.seek(0)
+	json.dump(full,f,indent=4)
+	f.truncate()
+
+xy = [[0]*(len(set(stageCountList))) for i in range(max(triggerList)+1)]
+for i in range(len(stageCountList)):
+    #print(i)
+    xy[triggerList[i]][stageCountList[i]-1] += 10
+	
+triggerBinCount = len(set(triggerList))
+stageBinCount = len(set(stageCountList))
+fig = plt.figure(figsize=(triggerBinCount+2,stageBinCount))
+plt.scatter(triggerList, stageCountList,s=np.array(xy).flatten())
+plt.locator_params(axis='x', nbins=triggerBinCount)
+plt.locator_params(axis='y', nbins=stageBinCount)
+imageTitle = 'Correlation coefficient: '+ str(corrCoef)
+plt.title(imageTitle)
+plt.xlabel('Number of triggers')
+plt.ylabel('Number of stages')
+fig.savefig('trig_stage_corr.jpg')
